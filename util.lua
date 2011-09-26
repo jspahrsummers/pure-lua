8aff2d350ada1f66b063d7878d07e781a49086b3
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-09-26 01:26:49 -0700

    Added generic tostring and eq metamethods for tables

--- util.lua
+++ util.lua
@@ -17,6 +17,33 @@ function util.table_copy_except (t, ...)
 	return setmetatable(u, getmetatable(t))
 end
 
+-- Returns the keys and values of 't' as a string.
+function util.table_tostring (t)
+	local str = "{ "
+
+	for k, v in pairs(t)
+	do
+		str = str .. tostring(k) .. " = " .. tostring(v) .. ", "
+	end
+
+	return str .. "}"
+end
+
+-- Compares the keys and values of 'tableA' and 'tableB', returning whether they were equal.
+function util.table_eq (tableA, tableB)
+	for k, v in pairs(tableA)
+	do
+		if v ~= tableB[k] then return false end
+	end
+
+	for k, v in pairs(tableB)
+	do
+		if v ~= tableA[k] then return false end
+	end
+
+	return true
+end
+
 -- Returns a deep copy of 'value'.
 function util.deep_copy (value)
 	if type(value) == "table" then
