ba3b48cd7707e30e0309c93cc728a4585216a6f7
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-09-27 20:42:23 -0700

    Sandbox all functions in the 'functional' module to be pure

--- functional.lua
+++ functional.lua
@@ -1,7 +1,9 @@
-functional = {}
+require('pure')
+
+local module = {}
 
 -- Maps 'func' across 'lst', passing each element into 'func' (in order), and constructing a new list from the results.
-function functional.map (lst, func)
+module.map = function (lst, func)
 	local result = {}
 
 	for i, v in ipairs(lst)
@@ -15,11 +17,14 @@ end
 -- Maps 'func' across 'table', passing every key and value into 'func'.
 -- 'func' should return a new key and a new value, which will be used to construct a new table from the results.
 -- Because the same key may be returned multiple times, the resultant table may contain fewer elements than the input table.
-function functional.map_pairs (table, func)
+module.map_pairs = function (table, func)
 	local result = {}
 
-	for k, v in pairs(lst)
+	for k, v in pairs(table)
 	do
+		local newKey
+		local newValue
+
 		newKey, newValue = func(k, v)
 		result[newKey] = newValue
 	end
@@ -28,7 +33,7 @@ function functional.map_pairs (table, func)
 end
 
 -- Returns a list of the elements from 'lst' for which 'func' returns a true value.
-function functional.filter (lst, func)
+module.filter = function (lst, func)
 	local result = {}
 
 	for i, v in ipairs(lst)
@@ -42,7 +47,7 @@ function functional.filter (lst, func)
 end
 
 -- Returns a table of the key/value pairs from 'table' for which 'func' returns a true value.
-function functional.filter_pairs (table, func)
+module.filter_pairs = function (table, func)
 	local result = {}
 
 	for k, v in pairs(table)
@@ -56,7 +61,7 @@ function functional.filter_pairs (table, func)
 end
 
 -- Curries 'func', returning a function that takes the first argument to 'func', and which returns a function taking all the other arguments.
-function functional.curry (func)
+module.curry = function (func)
 	return function (a)
 		return function (...)
 			return func(a, ...)
@@ -65,7 +70,7 @@ function functional.curry (func)
 end
 
 -- Binds the first arguments of 'func' to the given values. The first argument of 'bindr' is bound to the first argument of 'func', and so on.
-function functional.bindl (func, ...)
+module.bindl = function (func, ...)
 	local args = { ... }
 
 	return function (...)
@@ -74,7 +79,7 @@ function functional.bindl (func, ...)
 end
 
 -- Binds the last arguments of 'func' to the given values. The last argument of 'bindr' is bound to the last argument to 'func', and so on.
-function functional.bindr (func, ...)
+module.bindr = function (func, ...)
 	local args = { ... }
 
 	return function (...)
@@ -84,7 +89,7 @@ end
 
 -- For the given functor, returns a function which takes one more argument than 'func'.
 -- The additional argument will be passed into the function returned by 'func', and the result of that call returned.
-function functional.uncurry (func)
+module.uncurry = function (func)
 	return function(...)
 		local args = { ... }
 
@@ -92,3 +97,8 @@ function functional.uncurry (func)
 		return f(unpack(args, 2))
 	end
 end
+
+-- Sandbox and export the functions in this module under a 'functional' namespace
+functional = module.map_pairs(module, function (name, func)
+	return name, pure.sandbox(func)
+end)
